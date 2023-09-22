d8d198fffac3bf4081c99c077d38b206a38cd202
Shoalwave <shoalwave.dev@gmail.com>
2023-09-22 22:43:52 +0900

    [hyper] カラーの設定を Dracula のものに変更（プラグインで入れると背景色の透過が効かなくなってしまうため）

--- dot/.hyper.js
+++ dot/.hyper.js
@@ -20,7 +20,7 @@ module.exports = {
         // letter spacing as a relative unit
         letterSpacing: 0,
         // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-        cursorColor: 'rgba(248,28,229,0.8)',
+        cursorColor: '#f8f8f2',
         // terminal text color under BLOCK cursor
         cursorAccentColor: '#000',
         // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
@@ -28,14 +28,14 @@ module.exports = {
         // set to `true` (without backticks and without quotes) for blinking cursor
         cursorBlink: false,
         // color of the text
-        foregroundColor: '#fff',
+        foregroundColor: '#f8f8f2',
         // terminal background color
         // opacity is only supported on macOS
-        backgroundColor: 'rgba(22, 30, 38, 0.8)',
+        backgroundColor: 'rgba(40, 42, 54, 0.85)',
         // terminal selection color
         selectionColor: 'rgba(248,28,229,0.3)',
         // border color (window, tabs)
-        borderColor: '#333',
+        borderColor: '#44475a',
         // custom CSS to embed in the main window
         css: '',
         // custom CSS to embed in the terminal window
@@ -54,25 +54,23 @@ module.exports = {
         // the full list. if you're going to provide the full color palette,
         // including the 6 x 6 color cubes and the grayscale map, just provide
         // an array here instead of a color map object
-        colors: {
-            black: '#000000',
-            red: '#C51E14',
-            green: '#1DC121',
-            yellow: '#C7C329',
-            blue: '#0A2FC4',
-            magenta: '#C839C5',
-            cyan: '#20C5C6',
-            white: '#C7C7C7',
-            lightBlack: '#686868',
-            lightRed: '#FD6F6B',
-            lightGreen: '#67F86F',
-            lightYellow: '#FFFA72',
-            lightBlue: '#6A76FB',
-            lightMagenta: '#FD7CFC',
-            lightCyan: '#68FDFE',
-            lightWhite: '#FFFFFF',
-            limeGreen: '#32CD32',
-            lightCoral: '#F08080',
+        colors: { // dracula
+            black: "#000000",
+            red: "#ff5555",
+            green: "#50fa7b",
+            yellow: "#f1fa8c",
+            blue: "#bd93f9",
+            magenta: "#ff79c6",
+            cyan: "#8be9fd",
+            white: "#bfbfbf",
+            lightBlack: "#4d4d4d",
+            lightRed: "#ff6e67",
+            lightGreen: "#5af78e",
+            lightYellow: "#f4f99d",
+            lightBlue: "#caa9fa",
+            lightMagenta: "#ff92d0",
+            lightCyan: "#9aedfe",
+            lightWhite: "#e6e6e6",
         },
         // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
         // if left empty, your system's login shell will be used by default
@@ -169,7 +167,7 @@ module.exports = {
     localPlugins: [],
     keymaps: {
     // Example
-    // 'window:devtools': 'cmd+alt+o',
+    'window:devtools': 'cmd+alt+o',
     },
 };
 //# sourceMappingURL=config-default.js.map
